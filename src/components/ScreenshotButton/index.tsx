8588322db9ad58feb5f3803443949f3d65cfd2f7
FinotelliCarlos <carlos.finotelli@outlook.com>
2022-05-07 00:03:56 -0300

    finnaly component form

--- src/components/ScreenshotButton/index.tsx
+++ src/components/ScreenshotButton/index.tsx
@@ -1,6 +1,6 @@
 import { Camera, Trash } from 'phosphor-react-native'
 import React from 'react'
-import { TouchableOpacity, Image } from 'react-native'
+import { TouchableOpacity, Image, View } from 'react-native'
 import { theme } from '../../theme'
 
 import { styles } from './styles'
@@ -22,18 +22,17 @@ export function ScreenshotButton({
       onPress={screenshot ? onRemoveShot : onTakeShot}
     >
       {screenshot ? (
-        <Trash
-          size={22}
-          color={theme.colors.text_secondary}
-          weight={'fill'}
-          style={styles.removeIcon}
-        />
+        <View>
+          <Image style={styles.image} source={{ uri: screenshot }} />
+          <Trash
+            size={22}
+            color={theme.colors.text_secondary}
+            weight={'fill'}
+            style={styles.removeIcon}
+          />
+        </View>
       ) : (
-        <Camera 
-       size={24}
-       color={theme.colors.text_primary}
-       weight={'bold'}
-      />
+        <Camera size={24} color={theme.colors.text_primary} weight={'bold'} />
       )}
     </TouchableOpacity>
   )
